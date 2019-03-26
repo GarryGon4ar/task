74e9f53da1a79bcc6646208abdf929c2bf5a74db
GarryGon4ar <garrygon4ar@gmail.com>
2019-03-27 03:27:03 +0600

    Task

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic.base import TemplateView
+
+
+
+class HomePageView(TemplateView):
+
+    template_name = "base.html"
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/erl/Sunrise/task/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
